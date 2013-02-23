4f511aeda707a18db3eafb0261f6bbf72176a1c9
Sazaju HITOKAGE <sazaju@gmail.com>
2013-02-23 02:16:02 +0100

    Read and display all the parameters for the poker game.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+/**************************************\
+           INITIAL SETTING
+\**************************************/
 function error_handler($code, $message, $file, $line)
 {
     if (0 == error_reporting())
@@ -21,31 +24,81 @@ set_exception_handler('exception_handler');
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+/**************************************\
+           PARAMETERS READING
+\**************************************/
 $getParameter = function($name) {return isset($_REQUEST[$name]) ? $_REQUEST[$name] : null;};
 
-$gain1 = $getParameter('Gain1');
-$gain2 = $getParameter('Gain2');
+$gains = array();
+$gains[1] = $getParameter('Gain1');
+$gains[2] = $getParameter('Gain2');
 $game = $getParameter('Game');
 $tray = $getParameter('Tray');
 $turn = $getParameter('Turn');
-$move1 = $getParameter('Move1');
-$move2 = $getParameter('Move2');
+$moves = array();
+$moves[1] = $getParameter('Move1');
+$moves[2] = $getParameter('Move2');
 $moveId = $getParameter('MoveId');
 $opponent = $getParameter('Opponent');
 $referee = $getParameter('Referee');
 $set = $getParameter('Set');
 $timeout = $getParameter('TimeOut');
 
+$rank = substr($tray, 0, 2);
+$player = (($rank+1) % 2)+1;
+$resultedTurns = substr($tray, 2, 2);
+$step = substr($tray, 4, 1);
+$cards = array();
+$cards[1][1] = substr($tray, 5, 2);
+$cards[1][2] = substr($tray, 7, 2);
+$cards[2][1] = substr($tray, 9, 2);
+$cards[2][2] = substr($tray, 11, 2);
+$betStart = strpos($tray, 'M');
+$trayCards = array();
+for($i = 13 ; $i < $betStart ; $i += 2) {
+	$trayCards[] = substr($tray, $i, 2);
+}
+$isFirstToBet = substr($tray, $betStart + 1, 1) == '?';
+if ($isFirstToBet) {
+	$betStart ++;
+} else {
+	// no extra character.
+}
+$bets = array();
+$split = explode('-', substr($tray, $betStart + 1));
+$bets[1] = $split[0];
+$bets[2] = $split[1];
 
-// decision making
+/**************************************\
+                  GAME
+\**************************************/
 $login = 'tester';
 $value = mt_rand(1,3);
 
-// send results
+/**************************************\
+                 RESULTS
+\**************************************/
 if ($referee == null) {
 	$time = date('Y-m-d H:i:s', filemtime(__FILE__));
 	$commit = htmlspecialchars(exec('git log -1 --pretty=format:"%h - %s"'));
 	$source = $_SERVER['QUERY_STRING'];
+	echo "Gains: ".print_r($gains, true)."<br/>";
+	echo "Game: $game<br/>";
+	echo "Rank: $rank (player $player)<br/>";
+	echo "Resulted turns: $resultedTurns<br/>";
+	echo "Step: $step<br/>";
+	echo "Cards 1: ".print_r($cards[1], true)."<br/>";
+	echo "Cards 2: ".print_r($cards[2], true)."<br/>";
+	echo "Tray cards: ".print_r($trayCards, true)."<br/>";
+	echo "First to bet? ".($isFirstToBet ? 'Yes' : 'No')."<br/>";
+	echo "Bets: ".print_r($bets, true)."<br/>";
+	echo "Turn: $turn<br/>";
+	echo "Moves: ".print_r($moves, true)."<br/>";
+	echo "Move ID: $moveId<br/>";
+	echo "Opponent: $opponent<br/>";
+	echo "Set: $set<br/>";
+	echo "Timeout: $timeout<br/>";
+	echo "<br/>";
 	echo "Value: $value<br/>";
 	echo "<br/>";
 	echo "($time - $commit)<br/>";
